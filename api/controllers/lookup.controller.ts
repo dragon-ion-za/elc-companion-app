416bd048f65ee5fdd3eaaa5cf413af856d677e46
Ian Erasmus <ian.erasmus@entelect.co.za>
2024-02-01 10:59:43 +0200

    Added ID to the lookups, added more lookups
    #100

--- api/controllers/lookup.controller.ts
+++ api/controllers/lookup.controller.ts
@@ -5,13 +5,13 @@ import { LookupViewModel } from "../view-models/lookup.viewmodel";
 export class LookupController {
     public getRaces = (req: any, res: any) => {   
         let json = readFile(`${config.get("dataFileRoot")}races.json`);
-        let list = json.lookup.map((x: any) => new LookupViewModel(x.name));         
+        let list = json.lookup.map((x: any) => new LookupViewModel(x.id, x.name));         
         res.send(list);
     }
 
     public getFactions = (req: any, res: any) => {   
         let json = readFile(`${config.get("dataFileRoot")}factions.json`);
-        let list = json.lookup.map((x: any) => new LookupViewModel(x.name));         
+        let list = json.lookup.map((x: any) => new LookupViewModel(x.id, x.name));         
         res.send(list);
     }
 }
